f409b28ef535bc598816c378b5d3e923328dcbf0
Rod Boev <boevr@ruderfinn.com>
2018-04-24 13:47:07 -0400

    Remove Yuri

--- includes/contacts.php
+++ includes/contacts.php
@@ -1,6 +1,5 @@
     <div class="contacts">
         <a<?php if ($subpage === 'vikas-kapoor') echo $selected ?> href="/coordinates/vikas-kapoor/"><span>Vikas Kapoor</span> Chairman &amp; CEO</a>
         <a<?php if ($subpage === 'brad-olson') echo $selected ?> href="/coordinates/brad-olson/"><span>Brad Olson</span> President</a>
-        <a<?php if ($subpage === 'yuri-shtivelman') echo $selected ?> href="/coordinates/yuri-shtivelman/"><span>Yuri Shtivelman</span> Chief Operations Officer</a>
         <a<?php if ($subpage === 'leslie-stevens') echo $selected ?> href="/coordinates/leslie-stevens/"><span>Leslie Stevens</span> Chief Communications Officer</a>
     </div>
