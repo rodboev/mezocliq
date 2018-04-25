1f6067e50d4c3456bfacb0853caf7445c3a0635b
Rod Boev <boevr@ruderfinn.com>
2018-04-25 10:26:50 -0400

    Fix name highlighting

--- includes/contacts.php
+++ includes/contacts.php
@@ -1,5 +1,5 @@
     <div class="contacts">
         <a class="vikas<?php if ($subpage === 'vikas-kapoor') echo ' selected' ?>" href="/coordinates/vikas-kapoor/"><span>Vikas Kapoor</span> Chairman &amp; CEO</a>
-        <a class="brad<?php if ($subpage === 'vikas-kapoor') echo ' selected' ?>" href="/coordinates/brad-olson/"><span>Brad Olson</span> President</a>
-        <a class="leslie<?php if ($subpage === 'vikas-kapoor') echo ' selected' ?>" href="/coordinates/leslie-stevens/"><span>Leslie Stevens</span> Chief Communications Officer</a>
+        <a class="brad<?php if ($subpage === 'brad-olson') echo ' selected' ?>" href="/coordinates/brad-olson/"><span>Brad Olson</span> President</a>
+        <a class="leslie<?php if ($subpage === 'leslie-stevens') echo ' selected' ?>" href="/coordinates/leslie-stevens/"><span>Leslie Stevens</span> Chief Communications Officer</a>
     </div>
